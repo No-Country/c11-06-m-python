76dfaf291ee851b9971e140c6409d066ac49174c
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-05-17 13:04:33 -0500

    chore: Initialize shadcn UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Home = () => {
   return (
-    <main className="flex min-h-[calc(100vh-7rem)] flex-col items-center justify-between p-24">
+    <>
       Hola mundo
-    </main>
+    </>
   );
 };
 
