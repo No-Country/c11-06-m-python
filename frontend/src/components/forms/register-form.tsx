3dca995382bf627eb269fd8219cce427aa85474f
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-05-24 16:57:09 -0500

    add validaciones en register

--- frontend/src/components/forms/register-form.tsx
+++ frontend/src/components/forms/register-form.tsx
@@ -1,12 +1,25 @@
 "use client"
+import { RegisterFormSchema } from "@/lib/validations/form";
 import { Input } from "../ui/input";
+import { z } from "zod";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+
+type RegisterInput = z.infer<typeof RegisterFormSchema>;
 
 export const RegisterForm = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+
+  const {register, handleSubmit, formState: {errors} } = useForm<RegisterInput>({
+    resolver: zodResolver(RegisterFormSchema)
+  });
+
+  const onSubmit = (data:RegisterInput) => {
+    console.log(data);
   };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="py-11 flex flex-col gap-5">
         <label htmlFor="email">
           Correo electrónico
@@ -14,7 +27,9 @@ export const RegisterForm = () => {
             type="email"
             id="email"
             placeholder=""
+            {...register('email')}
           />
+          {errors?.email?.message && <p className="text-sm text-red-400 opacity-60">{errors.email.message}</p>}
         </label>
 
         <label htmlFor="password">
@@ -23,7 +38,9 @@ export const RegisterForm = () => {
             type="password"
             id="password"
             placeholder=""
+            {...register('password')}
           />
+          {errors?.password?.message && <p className="text-sm text-red-400 opacity-60">{errors.password.message}</p>}
         </label>
 
         <label htmlFor="password2">
@@ -32,7 +49,9 @@ export const RegisterForm = () => {
             type="password"
             id="password2"
             placeholder=""
+            {...register('repeatPassword')}
           />
+          {errors?.repeatPassword?.message && <p className="text-sm text-red-400 opacity-60">{errors.repeatPassword.message}</p>}
         </label>
 
         <div className="flex gap-5">
@@ -42,7 +61,9 @@ export const RegisterForm = () => {
               type="text"
               id="name"
               placeholder=""
+              {...register('name')}
             />
+            {errors?.name?.message && <p className="text-sm text-red-400 opacity-60">{errors.name.message}</p>}
           </label>
           <label htmlFor="lastname">
             Apellido
@@ -50,7 +71,9 @@ export const RegisterForm = () => {
               type="text"
               id="lastname"
               placeholder=""
+              {...register('lastname')}
             />
+            {errors?.lastname?.message && <p className="text-sm text-red-400 opacity-60">{errors.lastname.message}</p>}
           </label>
         </div>
 
@@ -60,7 +83,9 @@ export const RegisterForm = () => {
             type="date"
             id="birthdate"
             placeholder=""
+            {...register('birthdate')}
           />
+          {errors?.birthdate?.message && <p className="text-sm text-red-400 opacity-60">{errors.birthdate.message}</p>}
         </label>
       </div>
       <button className="px-12 w-full py-3 bg-slate-300 rounded-lg mb-7">
--- frontend/src/lib/validations/form.ts
+++ frontend/src/lib/validations/form.ts
@@ -3,11 +3,45 @@ import { z } from 'zod';
 export const LoginFormSchema = z.object({
     email: z
         .string()
-        .min(1, {message: 'Email obligatorio'})
+        .min(1, { message: 'Email obligatorio' })
         .email({
             message: 'Email no válido, Ingrese un email válido'
         }),
     password: z
         .string()
-        .min(1, {message: 'Contraseña obligatoria'}),
+        .min(1, { message: 'Contraseña obligatoria' }),
 })
+
+
+export const RegisterFormSchema = z.object({
+    email: z
+        .string()
+        .min(1, { message: 'Email obligatorio' })
+        .email({
+            message: 'Email no válido, Ingrese un email válido'
+        }),
+    password: z
+        .string()
+        .min(6, { message: 'La contraseña debe tener al menos 6 caracteres' }),
+    repeatPassword: z
+        .string()
+        .min(1, { message: 'Por favor, confirmar la contraseña' }),
+    name: z
+        .string()
+        .min(1, { message: 'Nombre obligatorio' }),
+    lastname: z
+        .string()
+        .min(1, { message: 'Apellido obligatorio' }),
+    birthdate: z.coerce
+        .date()
+        .min(new Date(1920, 0, 1), {
+            message: 'La fecha no puede superar el 1 de enero de 1920.'
+        })
+        .max(new Date(), {
+            message: "La fecha debe estar en el pasado."
+        })
+    })
+    .refine((data) => data.password === data.repeatPassword, {
+        path: ['repeatPassword'],
+        message: 'Las contraseñas con coinciden'
+    })
